34440765d46543cc422f0e05acc26c5058458cf2
fixmylifedesigns <ijd.irving@gmail.com>
2019-08-05 18:14:58 -0400

    blah

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,7 +10,7 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+print(sys.argv)
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 
@@ -23,9 +23,10 @@ import os
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print(os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+os.getcwd()
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
