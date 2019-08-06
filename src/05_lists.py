53ed47898aaafe338d930f5b58a7f99abfa8013c
fixmylifedesigns <ijd.irving@gmail.com>
2019-08-06 16:49:42 -0400

    11

--- src/05_lists.py
+++ src/05_lists.py
@@ -23,11 +23,15 @@ print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
+x.remove(10)
 x.append(99)
+x.append(10)
 print(x)
 
 # Print the length of list x
-# YOUR CODE HERE 
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+new_x = [i * 1000 for i in x]
+print(new_x)
